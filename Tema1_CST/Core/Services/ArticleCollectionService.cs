710400c7fb8bd4ed80009e001026de5aa352343a
Dinescu Damian <dandreidamian@gmail.com>
2023-03-29 16:31:21 +0300

    Add article service scaffolding.

---
+++ Tema1_CST/Core/Services/ArticleCollectionService.cs
@@ -0,0 +1,32 @@
+﻿using Tema1_CST.DataLayer.Entities;
+
+namespace Tema1_CST.Core.Services
+{
+    public class ArticleCollectionService : IArticleCollectionService
+    {
+        public Task<bool> Create(Article model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Article> Get(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Article>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Guid id, Article model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tema1_CST/Core/Services/IArticleCollectionService.cs
@@ -0,0 +1,9 @@
+﻿using Tema1_CST.DataLayer.Entities;
+
+namespace Tema1_CST.Core.Services
+{
+    public interface IArticleCollectionService : ICollectionService<Article>
+    {
+
+    }
+}
