5abf89e68df0c5fed7523eb1876a9ff83f73e061
Dinescu Damian <dandreidamian@gmail.com>
2023-03-29 17:28:24 +0300

    Add DataContext class.

---
+++ Tema1_CST/DataLayer/DataContext.cs
@@ -0,0 +1,25 @@
+﻿global using Microsoft.EntityFrameworkCore;
+using Tema1_CST.DataLayer.Entities;
+
+namespace Tema1_CST.DataLayer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=.;Database=Blog;Trusted_Connection=True;TrustServerCertificate=True");
+        }
+
+        // DbSets
+        public DbSet<Author> Authors { get; set; } = null!;
+        public DbSet<Article> Articles { get; set; } = null!;
+        public DbSet<Comment> Comments { get; set; } = null!;
+
+    }
+}
